f20fa49699401c6918fd0749fa93b91b14d0c1ca
Muhammed Mustafa Alnaddaf <alnaddafmustafa3@gamil.com>
2023-04-01 17:01:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(driversArray, string) {
+  return driversArray.filter(
+    (driversArray) => driversArray.toLowerCase() === string.toLowerCase()
+  );
+}
+
+function fuzzyMatch(driversArray, string) {
+  return driversArray.filter((driversArray) => driversArray.startsWith(string));
+}
+
+function matchName(driversArray, name) {
+  return driversArray.filter((driversArray) => driversArray.name === name);
+}
